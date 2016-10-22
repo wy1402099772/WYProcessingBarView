60348d39c51346d0481c86754d284d003975d7e4
wy1402099772 <1402099772@qq.com>
2016-10-22 13:25:33 +0800

    Initial Commit

---
+++ WYProcessingBarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WYProcessingBarView
+//
+//  Created by wyan assert on 2016/10/22.
+//  Copyright © 2016年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WYProcessingBarView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WYProcessingBarView
+//
+//  Created by wyan assert on 2016/10/22.
+//  Copyright © 2016年 wyan assert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
