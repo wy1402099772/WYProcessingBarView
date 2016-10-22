5b6a7dc94e901dcbcc834be94713b6ea56aa24a1
wy1402099772 <1402099772@qq.com>
2016-10-22 15:44:28 +0800

    finish

--- WYProcessingBarView/ViewController.m
+++ WYProcessingBarView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ProcessingBarView.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    ProcessingBarView *sub = [[ProcessingBarView alloc] initWithFrame:CGRectMake(0, 100, 300, 4)];
+    [self.view addSubview:sub];
 }
 
 
